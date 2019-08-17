0544ec1319b10edccb864ea7360bf3ae31140601
Meghan Stang <mstang15@gmail.com>
2019-08-17 12:26:36 -0600

    Add test for merchant show page

---
+++ spec/features/merchants/show_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'merchant show page', type: :feature do
+  describe 'As a user' do
+    before :each do
+      @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Richmond', state: 'VA', zip: 23137)
+    end
+
+    it 'I can see a merchants name, address, city, state, zip' do
+      visit "/merchants/#{@bike_shop.id}"
+
+      expect(page).to have_content("Brian's Bike Shop")
+      expect(page).to have_content("123 Bike Rd.\nRichmond, VA 23137")
+    end
+
+  end
+end
