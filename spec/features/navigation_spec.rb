a3c7b273970d7392cba4f09c3fe80a2c53375ab7
Meghan Stang <mstang15@gmail.com>
2019-08-17 15:23:56 -0600

    Add nav links for all merchants and all items

---
+++ spec/features/navigation_spec.rb
@@ -0,0 +1,22 @@
+
+require 'rails_helper'
+
+RSpec.describe 'Site Navigation' do
+  describe 'As a Visitor' do
+    it "I see a nav bar with links to all pages" do
+      visit '/merchants'
+
+      within 'nav' do
+        click_link 'All Items'
+      end
+
+      expect(current_path).to eq('/items')
+
+      within 'nav' do
+        click_link 'All Merchants'
+      end
+
+      expect(current_path).to eq('/merchants')
+    end
+  end
+end
