84f0645e7d79923b57c02412fa41b9906b33b098
Meghan Stang <mstang15@gmail.com>
2019-08-17 14:37:53 -0600

    Add routes for editing and updating items

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   get "/items", to: "items#index"
   get "/items/:id", to: "items#show"
+  get "/items/:id/edit", to: "items#edit"
+  patch "/items/:id", to: "items#update"
   get "/merchants/:merchant_id/items", to: "items#index"
   get "/merchants/:merchant_id/items/new", to: "items#new"
   post "/merchants/:merchant_id/items", to: "items#create"
