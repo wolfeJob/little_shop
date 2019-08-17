280e66cc72574f567e3cac27ab81781a6cb7801d
Meghan Stang <mstang15@gmail.com>
2019-08-17 12:40:07 -0600

    Remove comments

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -25,17 +25,3 @@ RSpec.describe 'merchant index page', type: :feature do
     end
   end
 end
-# When I visit the Merchant Index page
-# Then I see a link to create a new merchant
-# When I click this link
-# Then I am taken to '/merchants/new' where I  see a form for a new merchant
-# When I fill out the form with a new merchant's:
-# - name
-# - address
-# - city
-# - state
-# - zip
-# And I click the button to submit the form
-# Then a `POST` request is sent to '/merchants',
-# a new merchant is created,
-# and I am redirected to the Merchant Index page where I see the new Merchant listed.
