c9e9f1099d1615d0ca3f632c9eb33f060338451a
Meghan Stang <mstang15@gmail.com>
2019-08-17 12:39:40 -0600

    Add create action for merchants controller

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -11,4 +11,14 @@ class MerchantsController <ApplicationController
   def new
   end
 
+  def create
+    Merchant.create(merchant_params)
+    redirect_to "/merchants"
+  end
+
+  private
+
+  def merchant_params
+    params.permit(:name,:address,:city,:state,:zip)
+  end
 end
