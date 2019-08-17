29e8913542d17ce1a43f6d14593317efa0db5b22
Meghan Stang <mstang15@gmail.com>
2019-08-17 12:55:00 -0600

    Add test for deleting merchant

---
+++ spec/features/merchants/destroy_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "As a visitor" do
+  describe "When I visit a merchant show page" do
+    it "I can delete a merchant" do
+      bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Richmond', state: 'VA', zip: 80203)
+
+      visit "merchants/#{bike_shop.id}"
+
+      click_on "Delete Merchant"
+
+      expect(current_path).to eq('/merchants')
+      expect(page).to_not have_content("Brian's Bike Shop")
+    end
+  end
+end
