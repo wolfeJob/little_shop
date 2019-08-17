dca1ae3fbcf4b86732d468b15b816c422327ae8d
Meghan Stang <mstang15@gmail.com>
2019-08-17 14:56:20 -0600

    Add test for making sure merchant that has items can be deleted

--- spec/features/merchants/destroy_spec.rb
+++ spec/features/merchants/destroy_spec.rb
@@ -12,5 +12,17 @@ RSpec.describe "As a visitor" do
       expect(current_path).to eq('/merchants')
       expect(page).to_not have_content("Brian's Bike Shop")
     end
+
+    it "I can delete a merchant that has items" do
+      bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      chain = bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+
+      visit "merchants/#{bike_shop.id}"
+
+      click_on "Delete Merchant"
+
+      expect(current_path).to eq('/merchants')
+      expect(page).to_not have_content("Brian's Bike Shop")
+    end
   end
 end
