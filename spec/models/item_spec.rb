9e17e40f658fb79fdb3822ac2ad17466135a654d
Meghan Stang <mstang15@gmail.com>
2019-08-17 13:16:10 -0600

    Add tests for validations and relationships for items with merchants

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe Item, type: :model do
+  describe "validations" do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :price }
+    it { should validate_presence_of :image }
+    it { should validate_presence_of :inventory }
+    it { should validate_inclusion_of(:active?).in_array([true,false]) }
+  end
+
+  describe "relationships" do
+    it {should belong_to :merchant}
+  end
+end
