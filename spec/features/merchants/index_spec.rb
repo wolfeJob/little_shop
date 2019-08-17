5d3d2014f50c0d1a3728b9b85b470d480038a819
Meghan Stang <mstang15@gmail.com>
2019-08-17 12:32:23 -0600

    Add test for new merchant link on merchant index page

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -13,5 +13,29 @@ RSpec.describe 'merchant index page', type: :feature do
       expect(page).to have_content("Brian's Bike Shop")
       expect(page).to have_content("Meg's Dog Shop")
     end
+
+    it 'I can see a link to create a new merchant' do
+      visit '/merchants'
+
+      expect(page).to have_link("New Merchant")
+
+      click_on "New Merchant"
+
+      expect(current_path).to eq("/merchants/new")
+    end
   end
 end
+# When I visit the Merchant Index page
+# Then I see a link to create a new merchant
+# When I click this link
+# Then I am taken to '/merchants/new' where I  see a form for a new merchant
+# When I fill out the form with a new merchant's:
+# - name
+# - address
+# - city
+# - state
+# - zip
+# And I click the button to submit the form
+# Then a `POST` request is sent to '/merchants',
+# a new merchant is created,
+# and I am redirected to the Merchant Index page where I see the new Merchant listed.
