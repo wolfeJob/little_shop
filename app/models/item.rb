46df29073ef9429bd38fe067ab3fce865d327e10
Meghan Stang <mstang15@gmail.com>
2019-08-17 13:17:08 -0600

    Add belongs_to and validations for items

---
+++ app/models/item.rb
@@ -0,0 +1,11 @@
+class Item <ApplicationRecord
+  belongs_to :merchant
+
+  validates_presence_of :name,
+                        :description,
+                        :price,
+                        :image,
+                        :inventory
+  validates_inclusion_of :active?, :in => [true, false]
+
+end
