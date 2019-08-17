5421abe32f3a0f6461fe0515d3bf6538d4839895
Meghan Stang <mstang15@gmail.com>
2019-08-17 14:38:12 -0600

    Add edit and update actions for items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,6 +23,16 @@ class ItemsController<ApplicationController
     redirect_to "/merchants/#{merchant.id}/items"
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to "/items/#{item.id}"
+  end
+
   private
 
   def item_params
