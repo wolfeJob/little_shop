64f797300e8d0bd2975b0f8e07f5419007e9cfed
Meghan Stang <mstang15@gmail.com>
2019-08-17 12:38:55 -0600

    Add test for creating a new merchant

---
+++ spec/features/merchants/new_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe 'merchant new page', type: :feature do
+  describe 'As a user' do
+    it 'I can create a new merchant' do
+      visit '/merchants/new'
+
+      name = "Sal's Calz(ones)"
+      address = '123 Kindalikeapizza Dr.'
+      city = "Denver"
+      state = "CO"
+      zip = 80204
+
+      fill_in 'Name', with: name
+      fill_in 'Address', with: address
+      fill_in 'City', with: city
+      fill_in 'State', with: state
+      fill_in 'Zip', with: zip
+
+      click_button "Create Merchant"
+
+      new_merchant = Merchant.last
+
+      expect(current_path).to eq('/merchants')
+      expect(page).to have_content(name)
+      expect(new_merchant.name).to eq(name)
+      expect(new_merchant.address).to eq(address)
+      expect(new_merchant.city).to eq(city)
+      expect(new_merchant.state).to eq(state)
+      expect(new_merchant.zip).to eq(zip)
+    end
+
+  end
+end
