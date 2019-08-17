be4a425f9e462c7c0d1c589d1917e7bfd329ed1a
Meghan Stang <mstang15@gmail.com>
2019-08-17 12:33:07 -0600

    Add route for new merchant

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "/merchants", to: "merchants#index"
+  get "/merchants/new", to: "merchants#new"
   get "/merchants/:id", to: "merchants#show"
 end
