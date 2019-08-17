d0814c2d01ec157634d9c958557742cef743796e
Meghan Stang <mstang15@gmail.com>
2019-08-17 12:15:44 -0600

    Add test for merchant index

---
+++ spec/features/merchants/index_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'merchant index page', type: :feature do
+  describe 'As a user' do
+    before :each do
+      @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Richmond', state: 'VA', zip: 80203)
+      @dog_shop = Merchant.create(name: "Meg's Dog Shop", address: '123 Dog Rd.', city: 'Hershey', state: 'PA', zip: 80203)
+    end
+
+    it 'I can see a list of merchants in the system' do
+      visit '/merchants'
+
+      expect(page).to have_content("Brian's Bike Shop")
+      expect(page).to have_content("Meg's Dog Shop")
+    end
+  end
+end
