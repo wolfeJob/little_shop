84806f4709a1eb228015b217d9be59bfa1d36019
Meghan Stang <mstang15@gmail.com>
2019-08-18 21:03:56 -0600

    Change assertiong for css tag

--- spec/features/items/destroy_spec.rb
+++ spec/features/items/destroy_spec.rb
@@ -1,13 +1,3 @@
-# User Story 12, Item Delete
-#
-# As a visitor
-# When I visit an item show page
-# Then I see a link to delete the item
-# When I click the link
-# Then a 'DELETE' request is sent to '/items/:id',
-# the item is deleted,
-# and I am redirected to the item index page where I no longer see this item
-
 require 'rails_helper'
 
 RSpec.describe 'item delete', type: :feature do
@@ -23,7 +13,7 @@ RSpec.describe 'item delete', type: :feature do
       click_on "Delete Item"
 
       expect(current_path).to eq("/items")
-      expect("item-#{chain.id}").to be_present
+      expect(page).to_not have_css("#item-#{chain.id}")
     end
   end
 end
