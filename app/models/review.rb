59d51ae717423308451ea840a41f32af25694012
Christopher Kelling <cj.kelling88@gmail.com>
2019-08-21 05:46:55 -0600

    Validate 1 to 5 rating
    Co-authored-by: Scott Payton 33855435+wolfeJob@users.noreply.github.com

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,5 @@ class Review <ApplicationRecord
   validates_presence_of :title,
                         :content,
                         :rating
+  validates_inclusion_of :rating, in: (1..5)
 end
