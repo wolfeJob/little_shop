a4d3dceb644fae1a861edbb92c8fc6fd4d615c8b
Scott Payton <spayton.c@gmail.com>
2019-08-21 12:48:57 -0600

    Add route for carts controller

--- config/routes.rb
+++ config/routes.rb
@@ -23,4 +23,6 @@ Rails.application.routes.draw do
   get "/merchants/:merchant_id/items/new", to: "items#new"
   post "/merchants/:merchant_id/items", to: "items#create"
   delete "/items/:id", to: "items#destroy"
+  #cart routes
+  post "/cart", to: "carts#create"
 end
