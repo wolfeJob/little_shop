7f08f8034b1bbfc70fc3c2e1f7a55809e58f360e
Scott Payton <spayton.c@gmail.com>
2019-08-21 17:01:45 -0600

    Refactored carts controller and created helper method for cart, created PORO for cart and specs inside model and feature

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,13 +3,22 @@ include ActionView::Helpers::TextHelper
 
   def create #add item to cart
     @item = Item.find(params[:item_id])
-    item_id_str = @item.id.to_s
-    session[:cart] ||= Hash.new(0)
-    session[:cart][item_id_str] ||= 0
-    session[:cart][item_id_str] = session[:cart][item_id_str] + 1
-    quantity = session[:cart][item_id_str]
+    # @cart = Cart.new(session[:cart])
+    cart.add_item(@item.id)
+    session[:cart] ||= cart.contents
+    quantity = cart.count_of(@item.id)
     flash[:notice] = "You now have #{pluralize(quantity, "copy")} of #{@item.name} in your cart."
     redirect_to '/items'
+
+
+  #
+  #   song = Song.find(params[:song_id])
+  # @cart = Cart.new(session[:cart])
+  # @cart.add_song(song.id)
+  # session[:cart] = @cart.contents
+  # quantity = @cart.count_of(song.id)
+  # flash[:notice] = "You now have #{pluralize(quantity, "copy")} of #{song.title} in your cart."
+  # redirect_to songs_path
   end
 
 private
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,23 @@
 class Cart
+
+  attr_reader :contents
+
+  def initialize(initial_contents)
+     @contents = initial_contents || Hash.new(0)
+  end
+
+  def add_item(id)
+    @contents[id.to_s] = count_of(id) + 1
+  end
+
+  def total_count
+    @contents.values.sum
+  end
+
+  def count_of(id)
+      @contents[id.to_s].to_i
+  end
+
+
+
 end
