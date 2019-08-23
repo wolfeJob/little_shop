57b858ce65fa1ea339aeb63f04d4de9b7c4917a4
Scott Payton <spayton.c@gmail.com>
2019-08-22 21:38:49 -0600

    Fixed tests for cart and reviews, added flash message for reviews

--- spec/features/cart/show_spec.rb
+++ spec/features/cart/show_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
+include ActionView::Helpers::TextHelper
+
 describe 'Cart Show Page' do
  describe 'When I view my cart' do
    before :each do
      @merchant_1 = Merchant.create(name: 'Pie Shop', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80001)
      @merchant_2 = Merchant.create(name: 'Donut Shop', address: '321 Main St', city: 'Denver', state: 'CO', zip: 80233)
-     @pie = @merchant_1.items.create(name: 'Pie', description: "Yummy Pie!", price: 15, image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Tarte_aux_poires_2a.jpg/1920px-Tarte_aux_poires_2a.jpg(488 kB)
-https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Tarte_aux_poires_2a.jpg/1920px-Tarte_aux_poires_2a.jpg
-', inventory: 2 )
+     @pie = @merchant_1.items.create(name: 'Pie', description: "Yummy Pie!", price: 15, image:"https://images-na.ssl-images-amazon.com/images/I/511RQsDqUgL._SX522_.jpg", inventory: 2 )
      @donut = @merchant_2.items.create(name: 'Donut', description: "Delicious Donut!", price: 3, image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Glazed-Donut.jpg/1920px-Glazed-Donut.jpg(364 kB)
 https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Glazed-Donut.jpg/1920px-Glazed-Donut.jpg
 ', inventory: 10 )
@@ -25,9 +25,9 @@ https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Glazed-Donut.jpg/1920p
        expect(page).to have_link(@pie.name)
        expect(page).to have_css("img[src*='#{@pie.image}']")
        expect(page).to have_content("Sold by: #{@merchant_1.name}")
-       expect(page).to have_content("Price: #{number_to_currency(@pie.price)}")
+       expect(page).to have_content("Price: $#{(@pie.price)}")
        expect(page).to have_content("Quantity: 1")
-       expect(page).to have_content("Subtotal: #{number_to_currency(@pie.price)}")
+       expect(page).to have_content("Subtotal: $#{(@pie.price)}")
      end
      # within "#item-#{@donut.id}" do
      #   expect(page).to have_link(@merchant_2.name)
@@ -38,7 +38,7 @@ https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Glazed-Donut.jpg/1920p
      #   expect(page).to have_content("Quantity: 1")
      #   expect(page).to have_content("Subtotal: #{number_to_currency(@donut.price)}")
      # end
-     expect(page).to have_content("Total: #{number_to_currency((@pie.price))}")
+     expect(page).to have_content("Total: $#{((@pie.price))}")
    end
    it "I can view my cart when it is empty" do
      visit '/cart'
