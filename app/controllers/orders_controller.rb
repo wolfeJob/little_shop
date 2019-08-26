2965b0c27f9a3a4d4525433387756c080c111764
Scott Payton <spayton.c@gmail.com>
2019-08-25 19:52:04 -0600

    Created spec for order show page, view for order show, and added controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,10 @@ class OrdersController < ApplicationController
   def new
     @items = cart.display_cart
   end
+
+private
+
+  def order_params
+    params.permit(:username, :address, :city, :state, :zipcode)
+  end
 end
