a8802f171caad402f546e108323a03b47ab26868
Christopher Kelling <cj.kelling88@gmail.com>
2019-08-27 16:18:31 -0600

    User story 25 complete. Merchant statistics>
    Co-authored-by: Scott Payton 33855435+wolfeJob@users.noreply.github.com

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -8,12 +8,16 @@ class Merchant <ApplicationRecord
                         :state,
                         :zip
 
-def has_orders?
-   id = self.items.joins(:orders).pluck(:merchant_id)[0]
-   if id != nil
-     true
-   else
-     false
-   end
- end
+  def has_orders?
+    id = self.items.joins(:orders).pluck(:merchant_id)[0]
+    id != nil
+  end
+
+  def average_prices
+    items.average(:price)
+  end
+
+  def cities_shipped_to
+    self.items.joins(:orders).distinct.pluck(:city)
+  end
 end
